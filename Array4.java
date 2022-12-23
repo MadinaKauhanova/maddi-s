b17479ddaf2ebeff4461d91f50f9b4770fbc1f60
MadinaKauhanova <mkauhanova05@gmail.com>
2022-12-23 14:46:59 +0600

    maddi's

---
+++ Array4.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.util.Scanner;
+import java.io.PrintWriter;
+
+public class Array4 {
+	public static void main(String[] args) {
+		
+		int arr[] = {33,3,4,5};
+
+		for( int i:arr) {
+		System.out.println(i); 
+	
+        }
+	}
+}
